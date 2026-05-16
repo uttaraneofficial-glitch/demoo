bc22717eb11cf619303abba18fffe263443e6f5b
uttaraneofficial-glitch <uttaraneofficial@gmail.com>
2026-05-16 17:19:26 +0530

    Update RazorpayPlanProperties.java

--- starto-api/src/main/java/com/starto/config/RazorpayPlanProperties.java
+++ starto-api/src/main/java/com/starto/config/RazorpayPlanProperties.java
@@ -3,36 +3,31 @@ package com.starto.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-import jakarta.annotation.PostConstruct;
+
 @Data
 @Component
 @ConfigurationProperties(prefix = "razorpay")
 public class RazorpayPlanProperties {
 
-    private Key key;
-    private Plans plans;
+    private Key key = new Key();
+    private Plans plans = new Plans();
 
     @Data
     public static class Key {
-        private String id;
-        private String secret;
+        private String id = "dummy";
+        private String secret = "dummy";
     }
 
     @Data
     public static class Plans {
-        private String trial;
-        private String sprint;
-        private String boost;
-        private String pro;
-        private String captain;
-        private String captainPro;
-        private String proPlus;
-        private String growth;
-        private String annual;
+        private String trial = "plan_dummy";
+        private String sprint = "plan_dummy";
+        private String boost = "plan_dummy";
+        private String pro = "plan_dummy";
+        private String captain = "plan_dummy";
+        private String captainPro = "plan_dummy";
+        private String proPlus = "plan_dummy";
+        private String growth = "plan_dummy";
+        private String annual = "plan_dummy";
     }
-    @PostConstruct
-public void init() {
-    System.out.println("SPRING CONFIG LOADED:");
-    System.out.println("CAPTAIN PLAN = " + plans.getCaptain());
-}
 }
