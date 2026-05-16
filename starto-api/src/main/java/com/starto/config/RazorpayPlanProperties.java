edc855df06bdd55381623b6c4e676cfd3a526c62
uttaraneofficial-glitch <uttaraneofficial@gmail.com>
2026-05-16 17:16:25 +0530

    Update RazorpayPlanProperties.java

--- starto-api/src/main/java/com/starto/config/RazorpayPlanProperties.java
+++ starto-api/src/main/java/com/starto/config/RazorpayPlanProperties.java
@@ -4,43 +4,35 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import jakarta.annotation.PostConstruct;
-
 @Data
 @Component
 @ConfigurationProperties(prefix = "razorpay")
 public class RazorpayPlanProperties {
 
-    private Key key = new Key();
-    private Plans plans = new Plans();
+    private Key key;
+    private Plans plans;
 
     @Data
     public static class Key {
-        private String id = "dummy_key_id";
-        private String secret = "dummy_key_secret";
+        private String id;
+        private String secret;
     }
 
     @Data
     public static class Plans {
-        private String trial = "plan_dummy_trial";
-        private String sprint = "plan_dummy_sprint";
-        private String boost = "plan_dummy_boost";
-        private String pro = "plan_dummy_pro";
-        private String captain = "plan_dummy_captain";
-        private String captainPro = "plan_dummy_captain_pro";
-        private String proPlus = "plan_dummy_pro_plus";
-        private String growth = "plan_dummy_growth";
-        private String annual = "plan_dummy_annual";
+        private String trial;
+        private String sprint;
+        private String boost;
+        private String pro;
+        private String captain;
+        private String captainPro;
+        private String proPlus;
+        private String growth;
+        private String annual;
     }
-
     @PostConstruct
-    public void init() {
-        if (this.plans == null) {
-            this.plans = new Plans();
-        }
-        if (this.key == null) {
-            this.key = new Key();
-        }
-        System.out.println("SPRING CONFIG LOADED:");
-        System.out.println("CAPTAIN PLAN = " + plans.getCaptain());
-    }
+public void init() {
+    System.out.println("SPRING CONFIG LOADED:");
+    System.out.println("CAPTAIN PLAN = " + plans.getCaptain());
+}
 }
