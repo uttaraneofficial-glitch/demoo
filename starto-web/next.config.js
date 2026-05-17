6f6fd4ca1195e439ea2163a775dc395182cf8d41
uttaraneofficial-glitch <uttaraneofficial@gmail.com>
2026-05-17 13:16:04 +0530

    Update next.config.js

--- starto-web/next.config.js
+++ starto-web/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     domains: ['api.dicebear.com'],
     dangerouslyAllowSVG: true,
